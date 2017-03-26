ed099d6005c3fda0ef14e39190b0a81f017c2659
Yulle <yulguim@gmail.com>
2017-03-26 18:19:23 -0300

    i18n funcionando

--- tcc/src/main/resources/static/js/app/app@3.1.js
+++ tcc/src/main/resources/static/js/app/app@3.1.js
@@ -1,4 +1,13 @@
-var app = angular.module("App", ["ngSanitize", "ngRoute", 'ui.bootstrap', 'ui.select', 'ngTagsInput'])
+var app = angular.module("App", ["ngSanitize", "ngRoute", 'ui.bootstrap', 'ui.select', 'ngTagsInput', 'pascalprecht.translate'])
+
+.config(function($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy(null);
+    $translateProvider.useStaticFilesLoader({
+        prefix: '/js/i18n/locale-',
+        suffix: '.json'
+    });
+    $translateProvider.preferredLanguage('pt');
+});
 
 app.run(function($rootScope, $http, $timeout, $route, $location) {
     toastr.options.closeButton = true;
@@ -6,6 +15,10 @@ app.run(function($rootScope, $http, $timeout, $route, $location) {
 
 });
 
-app.controller("appCtrl", ["$scope", function ($scope) {
+app.controller("appCtrl", ["$scope", '$translate', function ($scope, $translate) {
+
+    $scope.changeLanguage = function(lang) {
+        $translate.use(lang);
+    }
 
 }]);
