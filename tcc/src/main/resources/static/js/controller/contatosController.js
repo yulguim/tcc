23ce41177521a8fb2218d63175bba8dc597d9bb7
Yulle <yulguim@gmail.com>
2017-05-29 23:07:40 -0300

    ajustes de interface

--- tcc/src/main/resources/static/js/controller/contatosController.js
+++ tcc/src/main/resources/static/js/controller/contatosController.js
@@ -6,6 +6,9 @@ app.controller("contatosCtrl", ["contatosService", function (contatosService) {
 	var iniciarTela = function() {
 		contatosService.list().success(function(view) {
 			vm.contatos = view;
+			vm.contatos.push(angular.copy(vm.contatos[0]));
+			vm.contatos.push(angular.copy(vm.contatos[0]));
+			vm.contatos.push(angular.copy(vm.contatos[0]));
 		});
 	};
 	
